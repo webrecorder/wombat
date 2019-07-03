1c555d4359defbd6045d72d651228a784e80e264
Ilya Kreymer <ikreymer@gmail.com>
2019-07-03 12:27:22 -0700

    fix parent override: - remove directly setting 'window.parent', as it may result in infinite loops if window object leaks because 'window.top != window.parent' when traversing hierarchy - fix override of 'window.parent' in object proxy, window.parent overriden only if window.top is also overriden

--- src/wombat.js
+++ src/wombat.js
@@ -5328,11 +5328,11 @@ Wombat.prototype.initWindowObjProxy = function($wbwindow) {
           case 'top':
             return wombat.$wbwindow.WB_wombat_top._WB_wombat_obj_proxy;
           case 'parent':
-            var realParent = $wbwindow.parent;
-            if (realParent === $wbwindow.WB_wombat_top) {
-              return $wbwindow.WB_wombat_top._WB_wombat_obj_proxy;
+            var parentProxy = wombat.$wbwindow.parent._WB_wombat_obj_proxy;
+            if (wombat.$wbwindow === wombat.$wbwindow.WB_wombat_top || !parentProxy) {
+              return wombat.$wbwindow.WB_wombat_top._WB_wombat_obj_proxy;
             }
-            return realParent._WB_wombat_obj_proxy;
+            return parentProxy;
         }
         return wombat.defaultProxyGet($wbwindow, prop, ownProps, funCache);
       },
@@ -5592,7 +5592,7 @@ Wombat.prototype.initTopFrame = function($wbwindow) {
 
   $wbwindow.__WB_replay_top = replay_top;
 
-  var real_parent = replay_top.__WB_orig_parent || replay_top.parent;
+  var real_parent = replay_top.parent;
   // Check to ensure top frame is different window and directly accessible (later refactor to support postMessage)
   if (real_parent == $wbwindow || !this.wb_info.is_framed) {
     real_parent = undefined;
@@ -5623,8 +5623,8 @@ Wombat.prototype.initTopFrame = function($wbwindow) {
         false
       );
     }
-    $wbwindow.__WB_orig_parent = $wbwindow.parent;
-    $wbwindow.parent = replay_top;
+    //$wbwindow.__WB_orig_parent = $wbwindow.parent;
+    //$wbwindow.parent = replay_top;
   }
 };
 
