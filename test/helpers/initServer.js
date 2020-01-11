ee9317ca47f77d010a4b673e0571e4a74cbee40d
Ilya Kreymer <ikreymer@gmail.com>
2020-01-10 20:23:14 -0800

    tweak test, fix service-worker-allowed header

--- test/helpers/initServer.js
+++ test/helpers/initServer.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs-extra');
 const createServer = require('fastify');
 
-const host = 'localhost';
+const host = '127.0.0.1';
 const port = 3030;
 const gracefullShutdownTimeout = 50000;
 const shutdownOnSignals = ['SIGINT', 'SIGTERM', 'SIGHUP'];
@@ -71,7 +71,7 @@ async function initServer() {
           .type('application/javascript; charset=UTF-8')
           .header(
             'Service-Worker-Allowed',
-            `${address}/live/20180803160549mp_/https://tests.wombat.io/`
+            `http://localhost:${port}/live/20180803160549mp_/https://tests.wombat.io/`
           )
           .send('console.log("hi")');
       }
